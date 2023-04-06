bf4bf8f68b370217bc9d4df230cf05cf80e43026
iwanicho <nichol999homework@gmail.com>
2023-04-06 16:43:34 +0900

    running and tested

---
+++ archive/init_dial.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+
+# Enable execute (run program) privilege for all related files
+sudo chmod +x dial.txt dial.sh init_dial.txt
+sleep 1
+# Install minicom package for sending AT command to SIM card
+sudo apt install minicom -y
+sleep 1
+# Run AT command in init_dial.txt through minicom to setup SIM Card's APN
+minicom -D /dev/ttyUSB2 -S /home/$(logname)/init_dial.txt
+sleep 1
+# Install 7zip package to extract .7z files
+sudo apt install p7zip-full -y
+sleep 1
+# Install kernel modules for the SIM Hat's driver
+sudo apt install raspberrypi-kernel-headers -y
+sleep 1
+# Install udhcpc package for connecting to the internet
+sudo apt install udhcpc -y
+sleep 1
+# Extract the zip file (.7z)
+sudo su -c "7z x SIM7600_NDIS.7z   -r -o./SIM7600_NDIS -y"
+sleep 1
+# Reset make files before compiling
+sudo su -c "cd /home/$(logname)/SIM7600_NDIS && make clean"
+sleep 1
+# Run make files to compile SIM Hat driver
+sudo su -c "cd /home/$(logname)/SIM7600_NDIS && make"
+sleep 1
+# Run dial.sh to configure internet data call
+sudo sh /home/pi/dial.sh
+sleep 1
+# Check whether the command line is already exist in /etc/rc.local
+if ! sudo grep -q "sudo sh /home/$(logname)/dial.sh &" /etc/rc.local; then
+    # Append the file into /etc/rc.local (1 line above "exit 0") to enable automatic run after reboot 
+    sudo sed -i "$(($(wc -l < /etc/rc.local)-0))i sudo sh /home/$(logname)/dial.sh &" /etc/rc.local
+fi
+sleep 1
+# Enable execute (run program) privilege /etc/rc.local
+sudo chmod +x /etc/rc.local
+sleep 1
+exit
